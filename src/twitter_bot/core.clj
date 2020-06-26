686e836f1aca822f8128ca5e609293029af6591c
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-06-25 22:45:25 -0400

    init commit

---
+++ src/twitter_bot/core.clj
@@ -0,0 +1,27 @@
+(ns twitter-bot.core
+  (:require [twitter.oauth :as oauth]
+            [twitter.api.restful :as rest]))
+;; => nil
+
+
+(defonce app-consumer-key         (System/getenv "TWITTER_CONSUMER_KEY"))
+(defonce app-consumer-secret      (System/getenv "TWITTER_CONSUMER_SECRET"))
+(defonce user-access-token        (System/getenv "TWITTER_ACCESS_TOKEN"))
+(defonce user-access-token-secret (System/getenv "TWITTER_ACCESS_TOKEN_SECRET"))
+
+
+(def creds (oauth/make-oauth-creds
+            app-consumer-key
+            app-consumer-secret
+            user-access-token
+            user-access-token-secret))
+
+(def messages ["Clojure is great!"
+               "I love Clojure."
+               "Clojure FTW!"])
+
+(defn send-tweet [msg creds]
+  (rest/statuses-update :oauth-creds creds :params {:status msg}))
+
+(defn -main []
+  (send-tweet (rand-nth messages) creds))
